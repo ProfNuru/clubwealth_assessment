6b0772932663a277da29e0ffe4eafc0631fd2154
Nurudeen Abdul-Karim <nurudeen.karim2016@gmail.com>
2023-02-05 14:52:15 +0000

    cat images handled

--- src/components/ui/RecordDetail/index.jsx
+++ src/components/ui/RecordDetail/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './RecordDetail.module.css'
 
 const RecordDetail = ({
@@ -7,6 +7,25 @@ const RecordDetail = ({
     removeRecordAction,
     subCategory
 }) => {
+    const [singleImageUrl, setSingleImageUrl] = useState({})
+    const [imagesArray, setImagesArray] = useState([])
+    const [fieldDataset, setFieldDataset] = useState({})
+
+    const getFieldDataset = async (url) => {
+        
+    }
+
+    const isImg = (field, value) => {
+        let test = /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(value)
+        return test
+    }
+
+    const isApi = (field, value) => {
+        console.log(field, value)
+        return false
+    }
+
+
   return (
     <div className={classes.recordModal}>
         <div className={classes.modalContent}>
@@ -21,11 +40,29 @@ const RecordDetail = ({
             <small style={{color:'#000',padding:'0px 10px'}}>This removes this record from the table</small>
             
             <div className={classes.detailsGrid}>
-            {Object.keys(selectedTableRow?.data).map((field,i)=><div 
-            key={i}
-            className={classes.detailFields}>
-                <p><span>{field.replaceAll('_',' ').replace(/([A-Z]+)/g, ' $1').trim()}</span>: {selectedTableRow?.data[field]}</p>
-            </div>)}
+            {Object.keys(selectedTableRow?.data).map((field,i)=>{
+                let fieldIsImg = isImg(field,selectedTableRow?.data[field])
+                if(fieldIsImg){
+                    return <div key={i} style={{
+                        gridColumn:'1 / span 1',
+                        gridRow:'1 / span 3'
+
+                    }}>
+                        <img style={{
+                            width:'100%',
+                            display:'block',
+                            borderRadius:'10px'
+                        }}
+                        src={selectedTableRow?.data[field]} 
+                        alt={`${field} image`} />
+                    </div>
+                }
+                return <div 
+                key={i}
+                className={classes.detailFields}>
+                    <p><span>{field.replaceAll('_',' ').replace(/([A-Z]+)/g, ' $1').trim()}</span>: {selectedTableRow?.data[field]}</p>
+                </div>
+            })}
             </div>
         </div>
     </div>
